d476a84a11f6a84bcc08f6b1a029c4339b5d2120
Alex <enjoy290178@gmail.com>
2022-03-17 23:11:33 +0100

    Ajout du bouton rejouer, ajout du footer

--- Home.js
+++ Home.js
@@ -2,6 +2,7 @@
 let choix = [];
 let repons = [];
 a = 0;
+z = 0
 let grille = document.getElementById('grille');
 
 
@@ -16,29 +17,40 @@ for (let i = 1; i < 26; i++) {
 
 function number(val) {
     let result = document.getElementById('result');
-    console.log(val);
+    let fin = document.getElementById('fin');
+    let zp = document.getElementById('z');
+    let again = document.createElement('input');
+
+    again.setAttribute('type','button');
+    again.setAttribute('value','Rejouer');
+    again.setAttribute('onclick','javascript:location.reload()');
+
     choix.push(val);
     result.innerHTML += choix[choix.length - 1] + " ";
+
     if (choix.length == 6) {
         document.body.removeChild(grille);
-        result.innerHTML += "Résultat : ";
+        document.body.appendChild(again);
+        fin.innerHTML = "Résultat : ";
+        zp.innerHTML = "Nombre égaux : " + z;
+
         var inter = setInterval(function () {
             let ran = Math.floor(Math.random() * 25);
             repons.push(ran);
-            result.innerHTML += repons[a] + " ";
+            fin.innerHTML += repons[a] + " ";
             a += 1;
             console.log(a)
-            if (repons.length == 6) { 
+            for (let j = 0; j < choix.length; j++) {
+                if (choix[j] == repons[repons.length - 1]) {
+                    z += 1;
+                    zp.innerHTML = "Nombre égaux : " + z;
+
+                };
+            };
+            if (repons.length == 6) {
                 clearInterval(inter)
             };
-
         }, 2000);
 
     };
 };
-
-function resultat(result) {
-
-
-}
-
